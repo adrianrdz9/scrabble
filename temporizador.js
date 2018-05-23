9b06912dbe1c461a38a6f406db6fbf2ebb27d5d1
Adrian Rodriguez <adrian.rodriguez7109@comunidad.unam.mx>
2018-05-22 20:11:51 -0500

    Temporizador

--- temporizador.js
+++ temporizador.js
@@ -1,6 +1,14 @@
 iniciarTemporizador(10, "h1");
 
-function formatearTemporizador(seg){
+/***************************************************************/
+/*    Dar formato mm:ss                                        */
+/*    Parametros:                                              */ 
+/* 1.- seg -> Segundos para dar formato, minimo 0, maximo 3599 */
+/*    Salida:                                                  */
+/*      Cadena ya con formato                                  */
+/*        Tipo -> string                                       */
+/************************************************************* */
+function formatearSegundo(seg){
     let min = Math.floor(seg / 60);
     let fseg = seg - (Math.floor(seg/60)*60 );
 
@@ -9,12 +17,24 @@ function formatearTemporizador(seg){
     return min + ":" + fseg;
 }
 
+/***************************************************************/
+/*    Iniciar el temporizador                                  */
+/*    Parametros:                                              */ 
+/* 1.- tiempo -> Segundos con los que se empieza,              */
+/*               minimo 0, maximo 3599                         */
+/* 2.- elemento -> Selector para el elemento donde             */ 
+/*                 se mostrara el tiempo                       */
+/* 3.- cb -> Funcion que se ejecutara cuando el                */
+/*           tiempo llegue a 0                                 */
+/*    Salida:                                                  */
+/*        Tipo -> undefined                                       */
+/************************************************************* */
 function iniciarTemporizador(tiempo, elemento, cb = console.log){
     document.tiempo = tiempo;
-    document.querySelector(elemento).innerHTML = formatearTemporizador(document.tiempo);
+    document.querySelector(elemento).innerHTML = formatearSegundo(document.tiempo);
     document.temporizador = setInterval(function(){
         document.tiempo--;
-        document.querySelector(elemento).innerHTML = formatearTemporizador(document.tiempo);
+        document.querySelector(elemento).innerHTML = formatearSegundo(document.tiempo);
         if(document.tiempo <= 0){
             clearInterval(document.temporizador);
             cb("Aqui se puede ejecutar la funcion necesaria de cuando se acaba el tiempo");
