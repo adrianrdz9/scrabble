af6dbb3a49ca01347192fa5ce3b70ddf894367cf
Adrian Rodriguez <adrian.rodriguez7109@comunidad.unam.mx>
2018-05-28 11:05:22 -0500

    Poner palabra en tablero

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 var canvas = document.querySelector("#tablero");
 var ctx = canvas.getContext("2d");
 var tamCuadro = canvas.width/15;
+ 
+var direccion = 0;
+
+$("#rotar").on("click", function(){
+    direccion ++;
+    if(direccion > 1)
+        direccion = 0;
+})
 
 function map(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
@@ -52,12 +60,20 @@ function tablero(){
             ctx.fillStyle = bgColor;
             ctx.fillRect(i*tamCuadro, j*tamCuadro, tamCuadro,tamCuadro);
             ctx.fillStyle = txtColor;
-            if(table[i][j].valores == "E"){
+            if(table[i][j].casilla.length == 1 ){
                 ctx.font = "24px Arial";
-                ctx.fillText("E", (i*tamCuadro)+tamCuadro/4.5, (j*tamCuadro)+tamCuadro/1.5 )
+                ctx.fillText(table[i][j].casilla, (i*tamCuadro)+tamCuadro/4.5, (j*tamCuadro)+tamCuadro/1.5 )
             }else{
-                wrapText(ctx, table[i][j].casilla, (i*tamCuadro)+5, (j*tamCuadro)+15, tamCuadro-5, 10  )
+                if(table[i][j].casilla == "Especial"){
+                    ctx.font = "24px Arial";
+                    ctx.fillText("E", (i*tamCuadro)+tamCuadro/4.5, (j*tamCuadro)+tamCuadro/1.5 )
+                }else{ 
+                    wrapText(ctx, table[i][j].casilla, (i*tamCuadro)+5, (j*tamCuadro)+15, tamCuadro-5, 10  )
+                }
             }
+            
+
+            
             ctx.restore()
         }
     }
@@ -79,26 +95,73 @@ function ponerPalabra(palabra){
         var x = ev.originalEvent.layerX;
         var y = ev.originalEvent.layerY;
         var letras = palabra.split('');
-        var posible = true;
-
+        var posible = false;
+        var l = [];
         letras.forEach(function(letra, i){
             var xi = x, yi = y;
             xi = Math.floor(map(xi, 0, canvas.width, 0, 15));
             yi = Math.floor(map(yi, 0, canvas.height, 0, 15));
 
-            if(table[xi+i][yi].casilla != "")
-                posible = false;
-        })
+            switch (direccion) {
+                case 0:
+                    xj = xi + i;
+                    yj = yi;
+                    break;
+            
+                case 1:
+                    xj = xi;
+                    yj = yi + i;
+                    break;
+            }
+            if(letra.toUpperCase() != table[xj][yj].casilla ){
+                l.push( table[xj][yj]);
+            }
+        });
+
+        l = l.filter((el)=>{
+            if(el.casilla == ""){
+                return false;
+            }else{
+                if(el.casilla == "Especial"){
+                    return false;
+                }else{
+                    if(el.casilla.length > 1){
+                        return false;
+                    }else{
+                        return true;
+                    }
+                }
+            }
+
+        });
+
+        if(l.length <= 0){
+            posible = true;
+        }
    
         letras.forEach(function(letra, i){
             x = Math.floor(map(x, 0, canvas.width, 0, 15))* tamCuadro;
             y = Math.floor(map(y, 0, canvas.height, 0, 15)) * tamCuadro;
+            
+            switch (direccion) {
+                case 0:
+                    xj = x + i*tamCuadro;
+                    yj = y;
+                    break;
+            
+                case 1:
+                    xj = x;
+                    yj = y+ i*tamCuadro;
+                    break;
+            }
+
+            
             ctx.save();
             ctx.fillStyle = posible ? "rgba(0,100,0,0.7)" : "rgba(255,0,0,0.7)";
-            ctx.fillRect(x+(i*tamCuadro), y, tamCuadro,tamCuadro );
+            ctx.fillRect(xj, yj, tamCuadro,tamCuadro );
             ctx.fillStyle = "white";
             ctx.font = "24px Arial";
-            ctx.fillText(letra.toUpperCase(), x+(i*tamCuadro)+tamCuadro/4.5, y+tamCuadro/1.5);
+            ctx.fillText(letra.toUpperCase(), xj+tamCuadro/4.5, yj+tamCuadro/1.5);
             ctx.restore();
         });
     });
@@ -112,15 +175,64 @@ function ponerPalabra(palabra){
         y = Math.floor(map(y, 0, canvas.height, 0, 15));
         
         var letras = palabra.split('');
-        var posible = true;
+        var posible = false;
 
+        var l = [];
         letras.forEach(function(letra, i){
-            if(table[x+i][y].casilla != "")
-                posible = false;
-        })
+            var xi = x, yi = y;
+
+            switch (direccion) {
+                case 0:
+                    xj = xi + i;
+                    yj = yi;
+                    break;
+            
+                case 1:
+                    xj = xi;
+                    yj = yi + i;
+                    break;
+            }
+            if(letra.toUpperCase() != table[xj][yj].casilla ){
+                l.push( table[xj][yj]);
+            }
+        });
+
+        l = l.filter((el)=>{
+            if(el.casilla == ""){
+                return false;
+            }else{
+                if(el.casilla == "Especial"){
+                    return false;
+                }else{
+                    if(el.casilla.length > 1){
+                        return false;
+                    }else{
+                        return true;
+                    }
+                }
+            }
+
+        });
+
+        if(l.length <= 0){
+            posible = true;
+        }
+
         if(posible){
             letras.forEach(function(letra, i){
-                table[x+i][y].casilla = letra;
+                switch (direccion) {
+                    case 0:
+                        xj = x + i;
+                        yj = y;
+                        break;
+                
+                    case 1:
+                        xj = x;
+                        yj = y+ i;
+                        break;
+                }
+
+                table[xj][yj].casilla = letra.toUpperCase();
             })
     
             ctx.clearRect(0,0,canvas.width, canvas.height);
